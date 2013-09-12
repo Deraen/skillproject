9525e23a5672c40ecaab14c0f1160cdcbcb81d18
Juho Teperi <juho.teperi@iki.fi>
2013-09-12 17:08:55 +0300

    cron script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#cron:
+# */5 * * * * /home/juho/skillproject/build.sh
+
+git fetch
+git reset --hard origin/hard
+
+git log -1 --format="%H" > newcommit
+
+if diff newcommit prevcommit >/dev/null; then echo "No changes"; exit; then
+
+npm install
+bower install
+grunt deploy
